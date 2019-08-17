64882bcb42c41d34b16185dfe769f23e87473cf8
Jeong Park <wjdtn1220@gmail.com>
2019-08-17 23:18:41 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,7 +1,7 @@
 def my_find(collection)
   i = 0
   while i < collection.length
-    return yield(collection[i]) if yield(collection[i])
-    i = i +1
+    return (collection[i]) if yield(collection[i])
+    i += 1
   end
 end
